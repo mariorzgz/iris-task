af57f2625533684802f4a4c5f8eb15fd024abbc6
mariorzgz <mario.rod.gonz97@gmail.com>
2022-10-11 12:17:49 +0200

    Started React

---
+++ my-app/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+
+      <header className="">
+      </header>
+
+      <main>
+
+        <section id="sidebar" className='sidebar'>
+
+        </section>
+
+        <section>
+          <h2>Search Results</h2>
+        </section>
+
+      </main>
+
+      <footer>
+
+      </footer>
+
+    </div>
+  );
+}
+
+export default App;
